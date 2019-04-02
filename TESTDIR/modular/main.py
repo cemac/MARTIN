31d2343917332067c3ec3315ef7530e35383c03c
hlburns <H.L.Burns@leeds.ac.uk>
2019-04-02 17:26:13 +0100

    :hammer_and_wrench: trying to edit source location

--- TESTDIR/modular/main.py
+++ TESTDIR/modular/main.py
@@ -1,9 +1,10 @@
 import tkinter as tk
-from tkinter import colorchooser, filedialog, ttk
+from tkinter import colorchooser, filedialog, ttk, StringVar
 import PIL.Image
 import PIL.ImageTk
 import PIL.ImageDraw
 import platform
+import os
 from statusbar import Statusbar
 from TKMARTINtools_dev import flat, resource_path
 plat = platform.system()
@@ -14,23 +15,79 @@ else:
 
 
 class Main(ttk.Frame):
-    def __init__(self, master):
+    def __init__(self, master, source_dir='.', run_after_selection=None):
         super().__init__()
+        self.fore_dict = {}
+        # define lists that will be used to populate dictionaries based on
+        # directory structure
+        self.master.source_dir = StringVar(self)
+        self.master.source_dir.set(source_dir)
+        source_list = next(os.walk(self.master.source_dir.get()))[1]
         region_list = []
         init_list = []
-        var_list = []
+        rr_list = []
         region_count = 0
         init_count = 0
         var_count = 0
+        # loop through top directory
+        for i in range(0, len(source_list)):
+            # check and populate first level forecast dictionary
+            if not source_list[i] in self.fore_dict:
+                self.fore_dict[source_list[i]] = {}
+            # define values in second level of directory structure and add to
+            # list for use in dictionary
+            region_list.append(next(os.walk(source_list[i] + "/."))[1])
+            # loop through second level of directories
+            for j in range(0, len(region_list[region_count])):
+                # check and populate second level forecast dictionary
+                if not region_list[region_count][j] in self.fore_dict[source_list[i]]:
+                    self.fore_dict[source_list[i]
+                                   ][region_list[region_count][j]] = {}
+                # define values in third level of directory structure and add
+                # to list for use in dictionary
+                init_list.append(next(os.walk(source_list[i] + "/" +
+                                      region_list[region_count][j] + "/."))[1])
+                # loop through third level of directories
+                for k in range(0, len(init_list[init_count])):
+                    # check and populate third level forecast dictionary
+                    if not init_list[init_count][k] in self.fore_dict[source_list[i]][region_list[region_count][j]]:
+                        self.fore_dict[source_list[i]][region_list[region_count]
+                                                       [j]][init_list[init_count][k]] = {}
+                    # define values in fourth level of directory structure and
+                    # add to list for use in dictionary
+                    var_list.append(next(os.walk(source_list[i] + "/" +
+                                                 region_list[region_count][j]
+                                                 + "/" +
+                                                 init_list[init_count][k] +
+                                                 "/."))[1])
+
+                    # loop through fourth level of directories
+                    for l in range(0, len(var_list[var_count])):
+                        temp_file_names = glob.glob(
+                            source_list[i] + "/" + region_list[region_count][j] + "/" + init_list[init_count][k] + "/" + var_list[var_count][l] + "/*")
+                        temp_fores_list = []
+                        for tfn in temp_file_names:
+                            tfn = os.path.basename(tfn)
+                            if "analysis" in tfn:
+                                temp_fores_list.append("000")
+                            if (tfn[-7:-4]).isnumeric():
+                                temp_fores_list.append(tfn[-7:-4])
+
+                        if not var_list[var_count][l] in self.fore_dict[source_list[i]][region_list[region_count][j]][init_list[init_count][k]]:
+                            self.fore_dict[source_list[i]][region_list[region_count][j]][init_list[init_count]
+                                                                                         [k]][var_list[var_count][l]] = list(sorted(set(temp_fores_list)))
+
+                    var_count = var_count + 1
+                init_count = init_count + 1
+            region_count = region_count + 1
         self.pack(padx=5, pady=5)
         self.FONT_SIZE = 12
         self.master.title("MARTIN - Image Viewer")
         self.open_file = ""
-        self.master.geometry("800x600")
-        self.master.tk_setPalette(background='#ececec')
+        self.tk_setPalette(background='#ececec')
         self.master.protocol('WM_DELETE_WINDOW', self.click_cancel)
-        self.master.bind('<Left>', self.previous)
-        self.master.bind('<Right>', self.next)
+        self.bind('<Left>', self.previous)
+        self.bind('<Right>', self.next)
         # Logo
         self.height = self.winfo_screenheight()
         logo_file = resource_path("MARTINlogo_small.png")
@@ -40,8 +97,144 @@ class Main(ttk.Frame):
         self.tklogo = PIL.ImageTk.PhotoImage(self.relogo)
         self.logo_label = tk.Label(self, image=self.tklogo)
         self.logo_label.grid(row=1, column=0, rowspan=3)
-        self.statusbar = Statusbar(self)
-        self.statusbar.set("This is the statusbar")
+        self.master.source_dir.trace('w', self.update_path)
+        self.optionmenu0 = tk.OptionMenu(self, self.master.source_dir.get(), *self.fore_dict.keys())
+        tk.Label(self, text="Source").grid(row=1, column=3, columnspan=3)
+        self.optionmenu0.grid(row=2, column=1, columnspan=3)
+        # Source drop down option box
+        self.var1 = tk.StringVar(self)
+        self.var1.trace('w', self.update_source)
+        self.optionmenu1 = tk.OptionMenu(self, self.var1, *self.fore_dict.keys())
+        tk.Label(self, text="Source").grid(row=1, column=3, columnspan=3)
+        self.optionmenu1.grid(row=2, column=3, columnspan=3)
+        # Region drop down option box
+        self.var2 = tk.StringVar(self)
+        self.var2.trace('w', self.update_region)
+        self.optionmenu2 = tk.OptionMenu(self, self.var2, " ")
+        tk.Label(self, text="Region").grid(row=1, column=6, columnspan=3)
+        self.optionmenu2.grid(row=2, column=6, columnspan=3)
+        # Init drop down option box
+        self.var3 = tk.StringVar(self)
+        self.var3.trace('w', self.update_init)
+        self.optionmenu3 = tk.OptionMenu(self, self.var3, " ")
+        tk.Label(self, text="Init").grid(row=1, column=9, columnspan=3)
+        self.optionmenu3.grid(row=2, column=9, columnspan=3)
+        # Var drop down option box
+        self.var4 = tk.StringVar(self)
+        self.var4.trace('w', self.update_var)
+        self.optionmenu4 = tk.OptionMenu(self, self.var4, " ")
+        tk.Label(self, text="Var").grid(row=1, column=12, columnspan=3)
+        self.optionmenu4.grid(row=2, column=12, columnspan=3)
+        # Previous button
+        self.previous_button = ttk.Button(self, text="<", command=self.previous)
+        self.previous_button.grid(row=2, column=15)
+        # Time drop down option box
+        self.var5 = tk.StringVar(self)
+        self.optionmenu5 = tk.OptionMenu(self, self.var5, " ")
+        tk.Label(self, text="Time").grid(row=1, column=16, columnspan=3)
+        self.optionmenu5.grid(row=2, column=16, columnspan=3)
+        # Next button
+        self.next_button = ttk.Button(self, text=">", command=self.next)
+        self.next_button.grid(row=2, column=19)
+        # Overlay button
+        self.var6 = tk.StringVar(self)
+        self.var6.trace('w', self.check_overlay)
+        self.optionmenu6 = tk.OptionMenu(self, self.var6, "")
+        tk.Label(self, text="Overlay").grid(row=1, column=20, columnspan=3)
+        self.optionmenu6.grid(row=2, column=20, columnspan=3)
+        #  Submit button
+        self.submit_button = ttk.Button(self, text="Submit",
+                                       command=self.check_vals)
+        self.submit_button.grid(row=2, column=23, columnspan=3)
+        # Select pen
+        self.pen_button = ttk.Button(self, text='pen', command=self.choose_pen)
+        self.pen_button.grid(row=3, column=3, columnspan=3)
+        # Select color for annotations
+        self.color_button = ttk.Button(self, text='colour',
+                                      command=self.choose_color)
+        self.color_button.grid(row=3, column=6, columnspan=3)
+        # Select pen thickness
+        self.thickness_button = tk.Scale(self, from_=1, to=10,
+                                         orient='horizontal',
+                                         label="Pen thickness")
+        self.thickness_button.set(5)
+        self.thickness_button.grid(row=3, column=9, columnspan=3)
+        # Undo button
+        self.undo_button = ttk.Button(self, text='undo', command=self.undo)
+        self.undo_button.grid(row=3, column=12, columnspan=3)
+        # Clear button
+        self.clear_button = ttk.Button(self, text='clear notes',
+                                      command=self.clear)
+        self.clear_button.grid(row=3, column=15, columnspan=3)
+        # Clear background button
+        self.clear_back_button = ttk.Button(self, text='clear background',
+                                           command=self.clear_back)
+        self.clear_back_button.grid(row=3, column=18, columnspan=3)
+        # Save with background button
+        self.save_all_button = ttk.Button(self, text='save',
+                               command=lambda: self.save_all_crop(self.canvas))
+        self.save_all_button.grid(row=3, column=21, columnspan=3)
+        # stamp button 1
+        self.conv_img1 = PIL.Image.open(resource_path("Conv_blk.png"))
+        self.conv_reimg1 = self.conv_img1.resize((30, 30))
+        self.conv_tkimg1 = PIL.ImageTk.PhotoImage(self.conv_reimg1)
+        self.conv_button1 = ttk.Button(self, image=self.conv_tkimg1,
+                           command=lambda: self.choose_stamp(self.conv_tkimg1))
+        self.conv_button1.grid(row=1, column=1)
+        # stamp button 2
+        self.conv_img2 = PIL.Image.open(resource_path("Conv_wht.png"))
+        self.conv_reimg2 = self.conv_img2.resize((30, 30))
+        self.conv_tkimg2 = PIL.ImageTk.PhotoImage(self.conv_reimg2)
+        self.conv_button2 = ttk.Button(self, image=self.conv_tkimg2,
+                           command=lambda: self.choose_stamp(self.conv_tkimg2))
+        self.conv_button2.grid(row=1, column=2)
+        # stamp button 3
+        self.conv_img3 = PIL.Image.open(resource_path("Conv_blk_grw.png"))
+        self.conv_reimg3 = self.conv_img3.resize((30, 30))
+        self.conv_tkimg3 = PIL.ImageTk.PhotoImage(self.conv_reimg3)
+        self.conv_button3 = ttk.Button(self, image=self.conv_tkimg3,
+                           command=lambda: self.choose_stamp(self.conv_tkimg3))
+        self.conv_button3.grid(row=2, column=1)
+        # stamp button 4
+        self.conv_img4 = PIL.Image.open(resource_path("Conv_wht_grw.png"))
+        self.conv_reimg4 = self.conv_img4.resize((30, 30))
+        self.conv_tkimg4 = PIL.ImageTk.PhotoImage(self.conv_reimg4)
+        self.conv_button4 = ttk.Button(self, image=self.conv_tkimg4,
+                           command=lambda: self.choose_stamp(self.conv_tkimg4))
+        self.conv_button4.grid(row=2, column=2)
+        # stamp button 5
+        self.conv_img5 = PIL.Image.open(resource_path("Conv_blk_dcy.png"))
+        self.conv_reimg5 = self.conv_img5.resize((30, 30))
+        self.conv_tkimg5 = PIL.ImageTk.PhotoImage(self.conv_reimg5)
+        self.conv_button5 = ttk.Button(self, image=self.conv_tkimg5,
+                           command=lambda: self.choose_stamp(self.conv_tkimg5))
+        self.conv_button5.grid(row=3, column=1)
+        # stamp button 6
+        self.conv_img6 = PIL.Image.open(resource_path("Conv_wht_dcy.png"))
+        self.conv_reimg6 = self.conv_img6.resize((30, 30))
+        self.conv_tkimg6 = PIL.ImageTk.PhotoImage(self.conv_reimg6)
+        self.conv_button6 = ttk.Button(self, image=self.conv_tkimg6,
+                           command=lambda: self.choose_stamp(self.conv_tkimg6))
+        self.conv_button6.grid(row=3, column=2)
+        # Setup image canvas
+        self.canvas = tk.Canvas(self.master, height=int(self.height * 0.666),
+                                width=self.height, bd=10, background='white')
+        self.canvas.pack()
+        # Input starting image
+        self.im = PIL.Image.open(resource_path("No_image_loaded.png"))
+        aspect_ratio = self.im.size[0] / self.im.size[1]
+        self.reim = self.im.resize((int(self.height * 0.666 * aspect_ratio),
+                                   int(self.height * 0.666)))
+        self.reim = self.im.resize((self.height, int(self.height * 0.666)))
+        self.tkim = PIL.ImageTk.PhotoImage(self.reim)
+        self.canvas.create_image(self.height / 2 + 10, int(self.height * 0.666)
+                                 / 2 + 10, image=self.tkim, tags="back")
+        # Setup label
+        self.status = tk.Label(self.master, text='Please make selection',
+                               bg='gray', font=('Helvetica', 15), bd=2,
+                               fg='black', relief='sunken', anchor='w')
+
+        self.setup()
 
     # Setup drawing on image
     def setup(self):
@@ -53,6 +246,75 @@ class Main(ttk.Frame):
         self.canvas.bind('<B1-Motion>', self.paint)
         self.canvas.bind('<ButtonRelease-1>', self.reset)
         self.linecount = 1
+
+    # Update dropdown menus based on selection of source
+    def update_path(self, *args):
+        source_list_new = []
+        source_list_new = next(os.walk(self.master.source_dir.get()))[1]
+        self.fore_dict = {}
+        region_list = []
+        init_list = []
+        rr_list = []
+        region_count = 0
+        init_count = 0
+        var_count = 0
+        # loop through top directory
+        for i in range(0, len(source_list_new)):
+            # check and populate first level forecast dictionary
+            if not source_list_new[i] in self.fore_dict:
+                self.fore_dict[source_list_new[i]] = {}
+            # define values in second level of directory structure and add to
+            # list for use in dictionary
+            region_list.append(next(os.walk(source_list_new[i] + "/."))[1])
+            # loop through second level of directories
+            for j in range(0, len(region_list[region_count])):
+                # check and populate second level forecast dictionary
+                if not region_list[region_count][j] in self.fore_dict[source_list_new[i]]:
+                    self.fore_dict[source_list_new[i]
+                                   ][region_list[region_count][j]] = {}
+                # define values in third level of directory structure and add
+                # to list for use in dictionary
+                init_list.append(next(os.walk(source_list_new[i] + "/" +
+                                      region_list[region_count][j] + "/."))[1])
+                # loop through third level of directories
+                for k in range(0, len(init_list[init_count])):
+                    # check and populate third level forecast dictionary
+                    if not init_list[init_count][k] in self.fore_dict[source_list[i]][region_list[region_count][j]]:
+                        self.fore_dict[source_list_new[i]][region_list[region_count]
+                                                       [j]][init_list[init_count][k]] = {}
+                    # define values in fourth level of directory structure and
+                    # add to list for use in dictionary
+                    var_list.append(next(os.walk(source_list_new[i] + "/" +
+                                                 region_list[region_count][j]
+                                                 + "/" +
+                                                 init_list[init_count][k] +
+                                                 "/."))[1])
+
+                    # loop through fourth level of directories
+                    for l in range(0, len(var_list[var_count])):
+                        temp_file_names = glob.glob(
+                            source_list_new[i] + "/" + region_list[region_count][j] + "/" + init_list[init_count][k] + "/" + var_list[var_count][l] + "/*")
+                        temp_fores_list = []
+                        for tfn in temp_file_names:
+                            tfn = os.path.basename(tfn)
+                            if "analysis" in tfn:
+                                temp_fores_list.append("000")
+                            if (tfn[-7:-4]).isnumeric():
+                                temp_fores_list.append(tfn[-7:-4])
+
+                        if not var_list[var_count][l] in self.fore_dict[source_list_new[i]][region_list[region_count][j]][init_list[init_count][k]]:
+                            self.fore_dict[source_list[i]][region_list[region_count][j]][init_list[init_count]
+                                                                                         [k]][var_list[var_count][l]] = list(sorted(set(temp_fores_list)))
+
+                    var_count = var_count + 1
+                init_count = init_count + 1
+            region_count = region_count + 1
+        sources = self.fore_dict[source_list_new]
+        menu = self.optionmenu1['menu']
+        menu.delete(0, 'end')
+        for source in sources:
+            menu.add_command(
+                label=source, command=lambda sou=source: self.var1.set(sou))
 
     # Update dropdown menus based on selection of source
     def update_source(self, *args):
--- TESTDIR/modular/toolbar.py
+++ TESTDIR/modular/toolbar.py
@@ -1,49 +1,73 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog
+import os
 
 
 class Toolbar(ttk.Frame):
     def __init__(self, master):
         super().__init__()
         self.menubar = tk.Menu(self, bg="lightgrey", fg="black")
+        # FILE
         self.file_menu = tk.Menu(
             self.menubar, tearoff=0, bg="lightgrey", fg="black")
         self.file_menu.add_command(
-            label="New", command=self.file_new, accelerator="Ctrl+N")
+            label="Source", command=self.file_new_source)
         self.file_menu.add_command(
-            label="Open", command=self.file_open, accelerator="Ctrl+O")
+            label="New Window", command=self.file_new_window)
         self.file_menu.add_command(
             label="Save", command=self.file_save, accelerator="Ctrl+S")
+        self.menubar.add_cascade(label="File", menu=self.file_menu)
+        # EDIT
         self.edit_menu = tk.Menu(
             self.menubar, tearoff=0, bg="lightgrey", fg="black")
         self.edit_menu.add_command(
-            label="Cut", command=self.edit_cut, accelerator="Ctrl+X")
+            label="Clear notes", command=self.edit_cut)
         self.edit_menu.add_command(
-            label="Paste", command=self.edit_paste, accelerator="Ctrl+V")
+            label="Clean background", command=self.edit_paste)
         self.edit_menu.add_command(
             label="Undo", command=self.edit_undo, accelerator="Ctrl+Z")
         self.edit_menu.add_command(
             label="Redo", command=self.edit_redo, accelerator="Ctrl+Y")
-        self.menubar.add_cascade(label="File", menu=self.file_menu)
         self.menubar.add_cascade(label="Edit", menu=self.edit_menu)
+        # View
+        self.view_menu = tk.Menu(
+            self.menubar, tearoff=0, bg="lightgrey", fg="black")
+        self.view_menu.add_command(
+            label="1-panel", command=self.panel_new)
+        self.view_menu.add_command(
+            label="2-panel", command=self.panel_split)
+        self.menubar.add_cascade(label="View", menu=self.view_menu)
+        # HELP
+        self.help_menu = tk.Menu(
+            self.menubar, tearoff=0, bg="lightgrey", fg="black")
+        self.menubar.add_cascade(label="Help", menu=self.help_menu)
+        self.master.configure(menu=self.menubar)
         self.master.configure(menu=self.menubar)
         # Bind Options
         self.bind("<Control-s>", self.file_save)
-        self.bind("<Control-o>", self.file_open)
-        self.bind("<Control-n>", self.file_new)
+        self.bind("<Control-n>", self.file_new_window)
         self.bind("<Control-v>", self.edit_paste)
 
-    def file_new(self, event=None):
-        file_name = filedialog.asksaveasfilename()
-        if file_name:
-            self.open_file = file_name
-            self.title(" - ".join([self.WINDOW_TITLE, self.open_file]))
+    def file_new_window(self, event=None):
+        return "break"
 
-    def file_open(self, event=None):
-        file_to_open = filedialog.askopenfilename()
-        if file_to_open:
-            self.open_file = file_to_open
-        self.title(" - ".join([self.WINDOW_TITLE, self.open_file]))
+    def file_new_source(self, event=None):
+        """
+        Called when users press the 'Browse' button in order to choose a path on their system.
+        """
+        cwd = os.getcwd()
+        new_path = None
+        new_path = filedialog.askdirectory(title=self.master.title,
+                                           initialdir=os.path.dirname(cwd),
+                                           mustexist=True,
+                                           parent=self.master)
+
+        # Updates the text in the Entry with the new path name.
+        if new_path:
+            self.master.source_dir.set(new_path)
+
+        return self.master.source_dir.set(new_path)
 
     def file_save(self, event=None):
         if not self.open_file:
@@ -85,3 +109,13 @@ class Toolbar(ttk.Frame):
         self.event_generate("<<Redo>>")
 
         return "break"
+
+    def panel_new(self, event=None):
+        return "break"
+
+    def panel_split(self, event=None):
+        return "break"
+
+    def on_key_release(self, event):
+        if not event.keysym in ("Up", "Down", "Left", "Right", "BackSpace", "Delete", "Escape"):
+            self.display_autocomplete_menu()
